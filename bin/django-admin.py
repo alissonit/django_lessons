272ee0366a9647b6476dfd8eb455d8e9ba1ca160
Alisson Castro <alissoncastroskt@gmail.com>
2020-05-13 17:09:51 -0300

    init repo

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alisson/Documents/workspace/django_digital_innovation/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
