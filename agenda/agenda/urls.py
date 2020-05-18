7ecc36cf81f9d135a336e2f0804ed8b2f28e37e6
Alisson Castro <alissoncastroskt@gmail.com>
2020-05-18 20:51:41 -0300

    ajusts login

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -21,5 +21,8 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', RedirectView.as_view(url="/agenda/"))
+    path('', RedirectView.as_view(url="/agenda/")),
+    path('login/', views.login_user),
+    path('login/entrar', views.entrar),
+    path('logout/', views.logout_user)
 ]
--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,15 +1,39 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from core.models import Evento
 
 # Create your views here.
 
 #def index(request):
 #   return redirect('/agenda/')
+
+def entrar(request):
+
+    if request.POST:
+        username = request.POST.get("user")
+        password = request.POST.get("password")
+        accept = authenticate(username=username, password=password)
+        if accept is not None:
+            login(request, accept)
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid user or password!")
+            
+    return redirect("/")
+
+def login_user(request):
+    return render(request, "login.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
+
+@login_required(login_url="/login/")
 def lista_eventos(request):
 
-    evento = Evento.objects.all()
-    data = {'eventos':evento}
+    username = request.user
+    evento = Evento.objects.filter(usuario=username)
+    data = {"eventos":evento}
     return render(request, "agenda.html", data)
-
-    
-
